4c25aaed0c4eac8a50a4d3456f0578fb61e1b9c5
Ragnar Lonn <ragnar@lonn.org>
2019-03-05 09:32:09 +0100

    Directory name is apparently vital. Very silly.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pushdata-io",
-    version="0.0.2",
+    version="0.0.3",
     author="Ragnar Lonn",
     author_email="hello@pushdata.io",
     description="Python client library for pushdata.io",
